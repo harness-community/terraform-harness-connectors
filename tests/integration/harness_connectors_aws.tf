0974e4b459d250dae2e29cac09aed7ae5dbda1a1
Your Name <you@example.com>
2023-03-27 14:19:16 -0600

    update tests

--- tests/integration/harness_connectors_aws.tf
+++ tests/integration/harness_connectors_aws.tf
@@ -1,33 +1,78 @@
-module "aws_connector_1" {
-  source = "../../../modules/aws/cloud"
-  identifier = "hello"
-  name="hello"
-
-  credentials = {
-    type="manual"
-    access_key="access_key"
-    secret_key_ref="secret_key_ref"
-    delegate_selectors=["delegate1","delegate2"]
-  }
-
-  cross_account_access = {
-    role_arn = "role_arn"
-  }
-  
+####################
+#
+# Harness Connectors AWS Validations
+#
+####################
+locals {
+  connectors_aws_outputs = flatten([
+    {
+      minimum                           = module.connector_aws_minimal.connector_details
+      manual_cross_account              = module.connector_aws_manual_cross_account.connector_details
+      inherit_no_cross_account          = module.connector_aws_inhert_no_cross_account.connector_details
+      connector_aws_irsa_cross_account  = module.connector_aws_irsa_cross_account.connector_details
+    }
+  ])
 }
 
-module "aws_connector_2" {
-  source = "../../../modules/aws/cloud"
-  identifier = "hello2"
-  name="hello2"
+module "connector_aws_minimal" {
+  source          = "../../modules/aws/cloud"
+  name            = "aws-manual-credentials-cross-account"
+  organization_id = local.organization_id
+  project_id      = local.project_id
 
   credentials = {
-    type="irsa"
-    delegate_selectors=["delegate1","delegate2"]
+    type               = "manual"
+    access_key         = "access_key"
+    secret_key_ref     = local.test_secret_name
+  }
+}
+
+
+module "connector_aws_manual_cross_account" {
+  source          = "../../modules/aws/cloud"
+  name            = "aws-manual-credentials-cross-account"
+  organization_id = local.organization_id
+  project_id      = local.project_id
+
+  credentials = {
+    type               = "manual"
+    access_key         = "access_key"
+    secret_key_ref     = local.test_secret_name
+    delegate_selectors = ["delegate1", "delegate2"]
   }
 
   cross_account_access = {
     role_arn = "role_arn"
   }
-  
+
+}
+
+module "connector_aws_inhert_no_cross_account" {
+  source          = "../../modules/aws/cloud"
+  name            = "aws-inherit-credentials-no-cross-account"
+  organization_id = local.organization_id
+  project_id      = local.project_id
+
+  credentials = {
+    type               = "inherit_from_delegate"
+    delegate_selectors = ["delegate1", "delegate2"]
+  }
+
+}
+
+module "connector_aws_irsa_cross_account" {
+  source          = "../../modules/aws/cloud"
+  name            = "aws-irsa-credentials"
+  organization_id = local.organization_id
+  project_id      = local.project_id
+
+  credentials = {
+    type               = "irsa"
+    delegate_selectors = ["delegate1", "delegate2"]
+  }
+
+  cross_account_access = {
+    role_arn = "role_arn"
+  }
+
 }
